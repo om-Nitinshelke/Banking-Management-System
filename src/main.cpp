1e743e4fa3f342d0230b66911f3d6dc914ff3075
Om <shelkeom67@gmail.com>
2026-09-11 13:02:16 +0530

    Add Phase 1 basic menu

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<<"Welcome to Banking Management System"<<endl;
+
+    cout<<"\n=====Main Menu====="<<endl;
+
+    cout<<"1. Create New Account"<<endl;
+    cout<<"2. Deposit Amount"<<endl;
+    cout<<"3. Withdraw Amount"<<endl;
+    cout<<"4. Check Balance"<<endl;
+    cout<<"5. Exit"<<endl;
+
+    return 0;
+}
